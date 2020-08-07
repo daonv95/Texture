283b895c67d45af6474e185b64f892af37b3e8ba
DaoNV <daonv@vng.com.vn>
2020-08-07 14:50:01 +0700

    Prevent CPU overload when use ASDisplayApply because of using dispatch_group to optimize parallel executions

--- Source/Private/ASDispatch.mm
+++ Source/Private/ASDispatch.mm
@@ -18,20 +18,24 @@
  * decides the system can't handle it. In reality this rarely happens.
  */
 void ASDispatchApply(size_t iterationCount, dispatch_queue_t queue, NSUInteger threadCount, NS_NOESCAPE void(^work)(size_t i)) {
-  if (threadCount == 0) {
-    threadCount = NSProcessInfo.processInfo.activeProcessorCount * 2;
-  }
-  dispatch_group_t group = dispatch_group_create();
-  __block atomic_size_t counter = ATOMIC_VAR_INIT(0);
-  for (NSUInteger t = 0; t < threadCount; t++) {
-    dispatch_group_async(group, queue, ^{
-      size_t i;
-      while ((i = atomic_fetch_add(&counter, 1)) < iterationCount) {
-        work(i);
-      }
+//  if (threadCount == 0) {
+//    threadCount = NSProcessInfo.processInfo.activeProcessorCount * 2;
+//  }
+//  dispatch_group_t group = dispatch_group_create();
+//  __block atomic_size_t counter = ATOMIC_VAR_INIT(0);
+//  for (NSUInteger t = 0; t < threadCount; t++) {
+//    dispatch_group_async(group, queue, ^{
+//      size_t i;
+//      while ((i = atomic_fetch_add(&counter, 1)) < iterationCount) {
+//        work(i);
+//      }
+//    });
+//  }
+//  dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
+    
+    dispatch_apply(iterationCount, queue, ^(size_t t) {
+        work(t);
     });
-  }
-  dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
 };
 
 /**
